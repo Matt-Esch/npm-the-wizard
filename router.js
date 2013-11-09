4483ab76c4d171cdddb11e90eecdacfa102edb72
Raynos <raynos2@gmail.com>
2013-11-08 20:45:28 -0800

    new routes

--- router.js
+++ router.js
@@ -2,12 +2,18 @@ var Router = require("routes-router")
 var ServeBrowserify = require("serve-browserify")
 var path = require("path")
 var st = require("st")
+var fs = require("fs")
+var sendHtml = require("send-data/html")
 
 var auth = require("./routes/auth.js")
+var indexHtml = fs.readFileSync(path.join(__dirname, "static", "index.html"))
 
 var router = Router()
 
 router.addRoute("/", function (req, res) {
+    sendHtml(req, res, indexHtml)
+
+
     // http://blog.nodeknockout.com/post/35364532732/protip-add-the-vote-ko-badge-to-your-app
     var voteko = "<iframe src='http://nodeknockout.com/iframe/team-mad-science' frameborder=0 scrolling=no allowtransparency=true width=115 height=25></iframe>"
 
