c1fe2ca107690898a26d75cf3e2920180776f0f3
Tim Caswell <tim@creationix.com>
2013-11-08 23:08:24 -0600

    Make log prettier

--- js-github/test.js
+++ js-github/test.js
@@ -1,8 +1,12 @@
 
   var log = console.log = console.error = require('domlog');
   log.setup({
-    whiteSpace: "pre",
+    whiteSpace: "pre-wrap",
     lineHeight: "1em",
+    transition: "inherit",
+    background: "#000",
+    top: "0",
+    height: "100%",
     overflow: "auto",
   });
 
@@ -25,7 +29,6 @@ repo.logWalk("HEAD", function (err, log) {
     // return log.read(onRead);
     repo.loadAs("tree", commit.tree, function (err, tree) {
         if (err) throw err;
-        console.log("TREE", tree);
         repo.treeWalk(commit.tree, function (err, tree) {
           if (err) throw err;
           tree.read(onEntry);
