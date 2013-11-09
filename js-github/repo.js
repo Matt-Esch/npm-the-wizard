6da98d08b74366cb7b58fccb813f28cf2cb32bb9
Tim Caswell <tim@creationix.com>
2013-11-08 21:50:04 -0600

    Hack in basic auth for easy testing

--- js-github/repo.js
+++ js-github/repo.js
@@ -2,11 +2,15 @@ module.exports = function (root) {
   var repo = {
     apiGet: apiGet
   };
+  var auth = document.location.search.substr(1);
+
+  if (!auth) throw "Please put http basic auth in search param";
 
   function apiGet(url, callback) {
     url = 'https://api.github.com' + url.replace(":root", root);
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
+    xhr.setRequestHeader("Authorization", "Basic " + auth);
     xhr.onreadystatechange = function () {
       if (xhr.readyState !== 4) return;
       if (xhr.status !== 200) {
