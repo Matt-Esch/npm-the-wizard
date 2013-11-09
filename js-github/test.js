0c91db67561fcac4cef224bde7596cdfecdeff15
Tim Caswell <tim@creationix.com>
2013-11-08 23:02:12 -0600

    Get walker working

--- js-github/test.js
+++ js-github/test.js
@@ -1,4 +1,11 @@
 
+  var log = console.log = console.error = require('domlog');
+  log.setup({
+    whiteSpace: "pre",
+    lineHeight: "1em",
+    overflow: "auto",
+  });
+
 var githubRepo = require('./repo.js');
 
 var repo = githubRepo("creationix/conquest");
@@ -7,15 +14,52 @@ console.log(repo);
 
 repo.logWalk("HEAD", function (err, log) {
   if (err) throw err;
+  var shallow;
   return log.read(onRead);
 
   function onRead(err, commit) {
     if (err) throw err;
-    if (!commit) return;
-    console.log(commit.hash, commit.author.name, commit.author.email, commit.author.date, commit.message);
-    log.read(onRead);
+    if (!commit) return logEnd(shallow);
+    if (commit.last) shallow = true;
+    logCommit(commit);
+    // return log.read(onRead);
+    repo.loadAs("tree", commit.tree, function (err, tree) {
+        if (err) throw err;
+        console.log("TREE", tree);
+        repo.treeWalk(commit.tree, function (err, tree) {
+          if (err) throw err;
+          tree.read(onEntry);
+          function onEntry(err, entry) {
+            if (err) throw err;
+            if (!entry) {
+              return log.read(onRead);
+            }
+            logEntry(entry);
+            return tree.read(onEntry);
+          }
+        });
+    });
   }
 });
+
+function logCommit(commit) {
+  var author = commit.author;
+  var message = commit.message;
+  console.log("commit " + commit.hash + "\n" +
+              "Author: " + author.name + " <" + author.email + ">\n" +
+              "Date:   " + author.date + "\n" +
+              "\n    " + message.trim().split("\n").join("n    ") + "\n");
+}
+
+function logEntry(entry) {
+  var path = entry.path.replace(/\//g, "/") + "";
+  console.log(" " + entry.hash + " " + path);
+}
+
+function logEnd(shallow) {
+  var message = shallow ? "End of shallow record." : "Beginning of history";
+  console.log("\n" + message + "\n");
+}
 // repo.loadAs("commit", "HEAD", function (err, commit) {
 //   if (err) throw err;
 //   console.log("HEAD", commit);
