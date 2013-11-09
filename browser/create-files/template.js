1ad3991493feb64e8ac3a0da68cf077ce957ac90
Raynos <raynos2@gmail.com>
2013-11-08 23:14:26 -0800

    fix template.js

--- browser/create-files/template.js
+++ browser/create-files/template.js
@@ -160,15 +160,17 @@ module.exports = {
       "}",
       ""
    ],
-   "test/index.js": [
-      "var test = require(\"tape\")",
-      "",
-      "var {{projectName}} = require(\"../index\")",
-      "",
-      "test(\"{{projectName}} is a function\", function (assert) {",
-      "    assert.equal(typeof {{projectName}}, \"function\")",
-      "    assert.end()",
-      "})",
-      ""
-   ]
+   "test": {
+      "index.js": [
+        "var test = require(\"tape\")",
+        "",
+        "var {{projectName}} = require(\"../index\")",
+        "",
+        "test(\"{{projectName}} is a function\", function (assert) {",
+        "    assert.equal(typeof {{projectName}}, \"function\")",
+        "    assert.end()",
+        "})",
+        ""
+      ]
+   }
 }
